11cc63e1db169235bf6d8b703f84bff50988c1a8
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-03-31 22:14:45 -0400

    Adds DistilBERT tokenizer and model and implements logistic regression classifier

--- main.py
+++ main.py
@@ -1,35 +1,68 @@
-# Import all the libraries needed
+# Imports all the libraries needed
 import pandas as pd
 import numpy as np
 import re
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import torch
+from transformers import DistilBertTokenizer, DistilBertModel
 
-# Loads dataset from a csv file (specificaly the columns "text and "label)
+# Loads dataset from a csv file (specifically the columns "text" and "label")
 def load_data(file_path):
     data = pd.read_csv(file_path)
     return data["text"], data["label"]
 
-# Filter the data
+# Filters the data
 def clean_text(text):
     if not isinstance(text, (str, bytes)): # if null returns empty string
         return ""
-    text = re.sub(r"[^a-zA-Z\s]", "", text) #using regex to remove special charachters
+    text = re.sub(r"[^a-zA-Z\s]", "", text) #using regex to remove special characters
     text = text.lower().strip()
     return text
 
+# Splits dataset
+def split_data(text, label, test_size=0.2):
+    data = pd.concat([text, label], axis=1)
+    train_data = data.sample(frac=1 - test_size, random_state=42)
+    test_data = data.drop(train_data.index)
+    return train_data["text"], test_data["text"], train_data["label"], test_data["label"]
 
-# Split dataset
-def split_data(text, label):
-    X_train, X_test, y_train, y_test = train_test_split(text, label, test_size=0.2, random_state=42)
-    return X_train, X_test, y_train, y_test
-
+# Tokenization and feature extraction
+def extract_features(texts, tokenizer, model, device):
+    features = []
+    for text in texts:
+        inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        with torch.no_grad():
+            outputs = model(**inputs)
+        embeddings = outputs.last_hidden_state.mean(dim=1).cpu().numpy()
+        features.append(embeddings[0])
+    return np.vstack(features)
 
 # Loads and preprocesses the data
-file_name = "train.csv" #change to file name
+file_name = "train.csv"
 text, label = load_data(file_name)
 text = text.apply(clean_text)
 X_train, X_test, y_train, y_test = split_data(text, label)
+
+# Initializes DistilBERT tokenizer and model
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Extracts features
+X_train_features = extract_features(X_train, tokenizer, model, device)
+X_test_features = extract_features(X_test, tokenizer, model, device)
+
+# Trains classifier
+clf = LogisticRegression(max_iter=100)
+clf.fit(X_train_features, y_train)
+
+# Evaluates model and prints stats
+y_pred = clf.predict(X_test_features)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average='weighted'))
+print("Recall:", recall_score(y_test, y_pred, average='weighted'))
+print("F1 Score:", f1_score(y_test, y_pred, average='weighted'))
 
